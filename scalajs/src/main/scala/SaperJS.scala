5b52fed15d3b026bcf7071f29cb297a7a19ef791
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-11-29 21:55:10 +0100

    gui display

--- scalajs/src/main/scala/SaperJS.scala
+++ scalajs/src/main/scala/SaperJS.scala
@@ -1,28 +1,76 @@
 import scala.scalajs.js.JSApp
 import org.scalajs.dom
-import org.scalajs.dom.{HTMLButtonElement, document}
+import org.scalajs.dom.{HTMLElement, HTMLAnchorElement, HTMLButtonElement, document}
 
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom.all._
 
-import saper._
+import saper.Saper._
 
 object SaperJS extends JSApp {
 
-  //val game
+  val config = GameConfig(5, 5, 5)
+  val game = Game(config)
 
+  def fieldByState(f: Field, v: FieldVisibility, x: Int, y: Int): HTMLAnchorElement = {
+    val fieldId = s"field-$x-$y"
+    val fieldCls = "btn btn-primary"
+
+    (v match {
+      case Closed =>
+        a(id := fieldId, cls := fieldCls)("?")
+      case Opened =>
+        f match {
+          case Empty =>
+            a(id := fieldId, cls := fieldCls)(".")
+          case Mine =>
+            a(id := fieldId, cls := "btn btn-danger")("*")
+          case Number(n) =>
+            a(id := fieldId, cls := "btn btn-success")(n)
+        }
+      case Flagged =>
+        a(id := fieldId, cls := "btn btn-info")("#")
+    }).render
+  }
+
+  def leftClick(x: Int, y: Int): Unit = {
+    game.click(x, y)
+    renderGame(gameDiv)
+  }
+
+  def rightClick(x: Int, y: Int): Unit = {
+    game.markFlag(x, y)
+    renderGame(gameDiv)
+  }
+
+  def renderGame(gameDiv: HTMLElement) = {
+    val rows = (0 until config.height).map { y =>
+      val buttons = (0 until config.width).map { x =>
+        val link = fieldByState(game.board((x, y)), game.visibilities((x, y)), x, y)
+        link.onclick = (e: dom.MouseEvent) => {
+          leftClick(x, y)
+        }
+        link.oncontextmenu = (e: dom.MouseEvent) => {
+          rightClick(x, y)
+        }
+        link
+      }
+      div(buttons)
+    }
+
+    gameDiv.innerHTML = ""
+    gameDiv.appendChild(div(rows).render)
+
+    gameDiv.appendChild(p(game.state.toString).render)
+
+    gameDiv.appendChild(p(s"Moves: ${game.moves}").render)
+  }
+
+
+  lazy val gameDiv = dom.document.getElementById("game")
 
   def main(): Unit = {
-
-    //dom.document.getElementById("game").innerHTML = "<h1>DUPA</h1>"
-
-    val config = Saper.GameConfig(10, 10, 10)
-    val board = Saper.Board(config)
-
-
-    println(board)
-
-
+    renderGame(gameDiv)
   }
 
 }
--- scalajvm/app/controllers/Application.scala
+++ scalajvm/app/controllers/Application.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import play.api.mvc._
-import hangman.Game
 
 object Application extends Controller {
 
